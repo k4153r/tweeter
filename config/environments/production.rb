61f93bc8e81bdf501238dc19df3369eafee61891
Decode 3000 <decode3000@googlemail.com>
2012-01-23 13:51:39 +0100

    added all assets that need to be precompiled

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -44,8 +44,9 @@ SampleApp::Application.configure do
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
-  config.assets.precompile += %w( blueprint/screen.css blueprint/print.css  blueprint/ie.css)
-
+  #config.assets.precompile += %w( blueprint/screen.css blueprint/print.css  blueprint/ie.css custom.css)
+  config.assets.precompile += Dir.glob(Rails.root.join("app/assets/**/*.css"))
+  
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
 
